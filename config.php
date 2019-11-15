19b3afff9c7bd5df50458c96b2e0a270d93a54e5
kyle lugo <kjl56@zips.uakron.edu>
2019-11-15 15:25:46 -0500

    change database connection
    connect to aws database instead of local database

--- config.php
+++ config.php
@@ -6,8 +6,9 @@
  
 /* Attempt to connect to MySQL database */
 //$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-$mysqli = new mysqli('localhost', 'root', '', 'isp_ys96');
- 
+//$mysqli = new mysqli('localhost', 'root', '', 'isp_ys96');
+$mysqli = mysqli_connect("database-2.cstibm4pl4qd.us-east-2.rds.amazonaws.com","admin","adminpassword","database_2",3306);
+
 // Check connection
 if($mysqli === false){
     die("ERROR: Could not connect. " . $mysqli->connect_error);
